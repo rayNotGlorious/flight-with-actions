eb785b01c8852fe586af3bf30e8d9e3d8f5ce09c
Akshay Patil <avpat02@gmail.com>
2023-02-12 14:02:10 -0500

    add code for sample state machine

--- src/flight_computer/flight_computer.rs
+++ src/flight_computer/flight_computer.rs
@@ -1,11 +1,11 @@
 #[derive(PartialEq, Debug)]
 struct FlightComputer {
-    shared_data: usize,
+    shared_data: i32,
     state: State,
 }
 
 impl FlightComputer {
-    pub fn new(shared_data: usize) -> FlightComputer {
+    pub fn new(shared_data: i32) -> FlightComputer {
         FlightComputer {
             shared_data,
             state: State::StateA(StateA {}),
@@ -13,26 +13,49 @@ impl FlightComputer {
     }
 
     pub fn next_state(self) -> Self {
-        match self.state {
+        let next_state = match self.state {
             State::StateA(val) => {
                 if self.shared_data > 0 {
-                    return FlightComputer {
-                        shared_data: self.shared_data,
-                        state: State::StateB(val.into()),
-                    };
+                    State::StateB(val.into())
+                } else {
+                    State::StateC(val.into())
                 }
             }
-            _ => {}
+            State::StateB(val) => {
+                if self.shared_data > 10 {
+                    State::StateD(val.into())
+                } else {
+                    State::StateB(val.into())
+                }
+            }
+            State::StateC(val) => {
+                if self.shared_data <= 10 {
+                    State::StateD(val.into())
+                } else {
+                    State::StateA(val.into())
+                }
+            }
+            State::StateD(val) => {
+                if self.shared_data > 100 {
+                    State::StateA(val.into())
+                } else {
+                    State::StateD(val.into())
+                }
+            }
+            _ => State::UnknownState,
+        };
+
+        FlightComputer {
+            shared_data: self.shared_data,
+            state: next_state,
         }
-        // TODO: remove this line
-        FlightComputer::new(1)
     }
 
     pub fn update_data(self) -> Self {
         return FlightComputer {
             shared_data: self.shared_data + 1,
             state: self.state,
-        }
+        };
     }
 }
 
@@ -40,6 +63,9 @@ impl FlightComputer {
 enum State {
     StateA(StateA),
     StateB(StateB),
+    StateC(StateC),
+    StateD(StateD),
+    UnknownState,
 }
 
 #[derive(PartialEq, Debug)]
@@ -48,9 +74,45 @@ pub struct StateA {}
 #[derive(PartialEq, Debug)]
 pub struct StateB {}
 
+#[derive(PartialEq, Debug)]
+pub struct StateC {}
+
+#[derive(PartialEq, Debug)]
+pub struct StateD {}
+
 impl From<StateA> for StateB {
-    fn from(_prev: StateA) -> Self {
+    fn from(_: StateA) -> Self {
         StateB {}
+    }
+}
+
+impl From<StateA> for StateC {
+    fn from(_: StateA) -> Self {
+        StateC {}
+    }
+}
+
+impl From<StateB> for StateD {
+    fn from(_: StateB) -> Self {
+        StateD {}
+    }
+}
+
+impl From<StateC> for StateD {
+    fn from(_: StateC) -> Self {
+        StateD {}
+    }
+}
+
+impl From<StateC> for StateA {
+    fn from(_: StateC) -> Self {
+        StateA {}
+    }
+}
+
+impl From<StateD> for StateA {
+    fn from(_: StateD) -> Self {
+        StateA {}
     }
 }
 
@@ -64,34 +126,117 @@ mod tests {
     #[test]
     fn given_state_a_when_shared_data_gt_0_then_state_b() {
         let shared_data = 1;
-        let flight_computer = FlightComputer::new(shared_data);
+        let actual_flight_computer = FlightComputer {
+            shared_data,
+            state: State::StateA(StateA {}),
+        };
         let expected_flight_computer = FlightComputer {
             shared_data,
             state: State::StateB(StateB {}),
         };
 
-        let flight_computer = flight_computer.next_state();
+        let actual_flight_computer = actual_flight_computer.next_state();
 
-        assert_eq!(flight_computer, expected_flight_computer);
+        assert_eq!(actual_flight_computer, expected_flight_computer);
     }
 
     // Given I am in state A
     // When shared_data <= 0
     // Then go to state C
+    #[test]
+    fn given_state_a_when_shared_data_lte_0_then_state_c() {
+        let shared_data: i32 = -1;
+        let actual_flight_computer = FlightComputer {
+            shared_data,
+            state: State::StateA(StateA {}),
+        };
+        let expected_flight_computer = FlightComputer {
+            shared_data,
+            state: State::StateC(StateC {}),
+        };
+
+        let actual_flight_computer = actual_flight_computer.next_state();
+
+        assert_eq!(actual_flight_computer, expected_flight_computer);
+    }
 
     // Given I am in state B
     // When shared_data > 10
     // Then go to state D
+    #[test]
+    fn given_state_b_when_shared_data_gt_10_then_state_d() {
+        let shared_data: i32 = 11;
+        let actual_flight_computer = FlightComputer {
+            shared_data,
+            state: State::StateB(StateB {}),
+        };
+        let expected_flight_computer = FlightComputer {
+            shared_data,
+            state: State::StateD(StateD {}),
+        };
+
+        let actual_flight_computer = actual_flight_computer.next_state();
+
+        assert_eq!(actual_flight_computer, expected_flight_computer);
+    }
 
     // Given I am in state C
     // When shared_data <= 10
     // Then go to state D
+    #[test]
+    fn given_state_c_when_shared_data_lte_10_then_state_d() {
+        let shared_data: i32 = 9;
+        let actual_flight_computer = FlightComputer {
+            shared_data,
+            state: State::StateC(StateC {}),
+        };
+        let expected_flight_computer = FlightComputer {
+            shared_data,
+            state: State::StateD(StateD {}),
+        };
+
+        let actual_flight_computer = actual_flight_computer.next_state();
+
+        assert_eq!(actual_flight_computer, expected_flight_computer);
+    }
 
     // Given I am in state C
     // When shared_data > 10
     // Then go to state A
+    #[test]
+    fn given_state_c_when_shared_data_gt_10_then_state_a() {
+        let shared_data: i32 = 11;
+        let actual_flight_computer = FlightComputer {
+            shared_data,
+            state: State::StateC(StateC {}),
+        };
+        let expected_flight_computer = FlightComputer {
+            shared_data,
+            state: State::StateA(StateA {}),
+        };
+
+        let actual_flight_computer = actual_flight_computer.next_state();
+
+        assert_eq!(actual_flight_computer, expected_flight_computer);
+    }
 
     // Given I am in state D
     // When shared_data > 100
     // Then go to state A
+    #[test]
+    fn given_state_d_when_shared_data_gt_100_then_state_a() {
+        let shared_data: i32 = 101;
+        let actual_flight_computer = FlightComputer {
+            shared_data,
+            state: State::StateD(StateD {}),
+        };
+        let expected_flight_computer = FlightComputer {
+            shared_data,
+            state: State::StateA(StateA {}),
+        };
+
+        let actual_flight_computer = actual_flight_computer.next_state();
+
+        assert_eq!(actual_flight_computer, expected_flight_computer);
+    }
 }
